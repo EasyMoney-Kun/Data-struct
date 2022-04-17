664ca43cf96caa1ed0767e0f3bb5be03b87ac1be
EasyMoney12138 <2967099002@qq.com>
2022-04-17 13:42:07 +0800

    更新

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Data-struct
-数据结构学习过程记录
+# :notebook_with_decorative_cover:数据结构
+## [:smile:基本概念](数据结构/基本概念.md)
+## [:smiley:线性表](数据结构/线性表.md)
+## [:laughing:栈和队列](数据结构/栈和队列.md)
+
--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,7 @@
-for (i = 1; i < = n; i++)    // n+1次
-    for (j = 1; j <= n; j++) // n*n+1次
-    {
-        c[i][j] = 0;
-        for (k = 0; k < n; k++) // n*n*n+1次
-            c[i][j] = c[i][j] + a[i][k] * b[k][j]; // n*n*n次
-    }
+Status GetHead(LinkQueue Q, QElemType &e)
+{
+    if (Q.front == Q.rear)
+        return ERROR;
+    e = Q.front->next->data;
+    return OK;
+}
