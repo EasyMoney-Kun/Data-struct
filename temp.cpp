84bb9a4055a7f96c25d5d6c50ea170d8338e6ba6
EasyMoney12138 <2967099002@qq.com>
2022-05-05 21:19:58 +0800

    内容增加

--- temp.cpp
+++ temp.cpp
@@ -1,7 +1,24 @@
-Status GetHead(LinkQueue Q, QElemType &e)
+void get_next(SString T, int next)
 {
-    if (Q.front == Q.rear)
-        return ERROR;
-    e = Q.front->next->data;
-    return OK;
+    next[1] = 0;
+    next[2] = 1;
+    int i = 2;
+    while (i < length)
+    {
+        if (T.ch[i] == T.ch[next[i]])
+        {
+            next[++i] = next[i] + 1;
+        }
+        else
+        {
+            if (T.ch[i] == T.ch[1])
+            {
+                next[++i] = 2;
+            }
+            else
+            {
+                next[++i] = 1;
+            }
+        }
+    }
 }
